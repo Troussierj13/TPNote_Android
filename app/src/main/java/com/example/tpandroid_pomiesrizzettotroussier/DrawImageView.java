81348f595fd5026369908260275df57f2bca99b7
troussier <troussier.julien@gmail.com>
2019-11-04 10:45:35 -0500

    Ajout d'un image qui est scalable avec la detection des gestes.

--- app/src/main/java/com/example/tpandroid_pomiesrizzettotroussier/DrawImageView.java
+++ app/src/main/java/com/example/tpandroid_pomiesrizzettotroussier/DrawImageView.java
@@ -10,19 +10,22 @@ import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
+import android.view.Display;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.ScaleGestureDetector;
 import android.view.View;
 
-
+import java.util.ArrayList;
 
 
 public class DrawImageView extends View {
 
     private float mScale = 1f;
     private Paint mPaint;
+    private Paint mPaintDraw;
     private float mFontSize;
+    private Bitmap mDraw;
 
     private ScaleGestureDetector mScaleGestureDetector;
 
@@ -34,14 +37,22 @@ public class DrawImageView extends View {
         mPaint.setColor(Color.RED);
         mPaint.setTextSize(mFontSize);
 
+        mPaintDraw = new Paint(Paint.DITHER_FLAG);
+
+        mDraw = BitmapFactory.decodeResource(getResources(), R.drawable.img);
+
         mScaleGestureDetector = new ScaleGestureDetector(context, new ScaleGesture());
+
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        canvas.drawText("texte",10, 20, mPaint);
-        canvas.drawText("texte",10, 60, mPaint);
+        //canvas.drawText("texte",10, 20, mPaint);
+        //canvas.drawText("texte",10, 60, mPaint);
+
+        Bitmap tmp = Bitmap.createScaledBitmap(mDraw, (int) (480f * mScale), (int) (320f * mScale), false);
+        canvas.drawBitmap(tmp, 0, 0, mPaintDraw);
 
     }
 
