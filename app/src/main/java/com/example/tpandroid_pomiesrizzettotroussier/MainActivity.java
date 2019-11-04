0378313d08aae8bdec7de945ef3a1012752ee275
Matthieu RIZZETTO <matthieurizzetto@yahoo.fr>
2019-11-04 17:06:16 +0100

    (MR) : Gesture

--- app/src/main/java/com/example/tpandroid_pomiesrizzettotroussier/MainActivity.java
+++ app/src/main/java/com/example/tpandroid_pomiesrizzettotroussier/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.tpandroid_pomiesrizzettotroussier;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Paint;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,4 +14,12 @@ public class MainActivity extends AppCompatActivity {
         DrawImageView view = new DrawImageView(this);
         setContentView(view);
     }
+
+    /**
+     * Display display = getWindowManager(). getDefaultDisplay();
+     * Point size = new Point();
+     * display. getSize(size);
+     * int width = size. x;
+     * int height = size. y;
+     */
 }
