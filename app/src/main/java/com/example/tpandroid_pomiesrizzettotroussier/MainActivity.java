f57299e0563efa6c1a0a6d4aca3f210da1774c15
Matthieu RIZZETTO <matthieurizzetto@yahoo.fr>
2019-11-06 15:16:24 +0100

    Merge branch 'master' of C:\Users\MR\AndroidStudioProjects\TPNote_Android with conflicts.

--- app/src/main/java/com/example/tpandroid_pomiesrizzettotroussier/MainActivity.java
+++ app/src/main/java/com/example/tpandroid_pomiesrizzettotroussier/MainActivity.java
@@ -1,23 +1,29 @@
 package com.example.tpandroid_pomiesrizzettotroussier;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Paint;
 import android.graphics.Point;
 import android.os.Bundle;
 import android.view.Display;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
 
         Display display = getWindowManager().getDefaultDisplay();
         Point size = new Point();
         display.getSize(size);
-        int width = size. x;
-        int height = size. y;
+        int width = size.x;
+        int height = size.y;
 
-        DrawImageView view = new DrawImageView(this, width, height);
+        super.onCreate(savedInstanceState);
+        DrawImageView view = new DrawImageView(this);
         setContentView(view);
     }
+
+
+
 }
